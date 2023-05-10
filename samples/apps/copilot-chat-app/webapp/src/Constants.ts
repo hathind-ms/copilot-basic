a3e09f5340d0b55efa86a36e01ce1189c8e215b8
Harleen Thind <hathind@microsoft.com>
2023-05-09 18:07:02 -0700

    add folder back

---
+++ samples/apps/copilot-chat-app/webapp/src/Constants.ts
@@ -0,0 +1,45 @@
+export const Constants = {
+    app: {
+        name: 'Copilot',
+        updateCheckIntervalSeconds: 60 * 5,
+    },
+    msal: {
+        method: 'redirect', // 'redirect' | 'popup'
+        auth: {
+            clientId: process.env.REACT_APP_AAD_CLIENT_ID as string,
+            authority: process.env.REACT_APP_AAD_AUTHORITY as string,
+        },
+        cache: {
+            cacheLocation: 'localStorage',
+            storeAuthStateInCookie: false,
+        },
+        skScopes: ['openid', 'offline_access', 'profile'],
+    },
+    bot: {
+        profile: {
+            id: 'bot',
+            fullName: 'Copilot',
+            emailAddress: '',
+            photo: '/assets/bot-icon-1.png',
+        },
+        fileExtension: 'skcb',
+        typingIndicatorTimeoutMs: 5000,
+    },
+    debug: {
+        root: 'sk-chatbot',
+    },
+    sk: {
+        service: {
+            defaultDefinition: 'int',
+        },
+    },
+    // For a list of Microsoft Graph permissions, see https://learn.microsoft.com/en-us/graph/permissions-reference.
+    // Your application registration will need to be granted these permissions in Azure Active Directory.
+    msGraphScopes: [
+        'Calendars.Read',
+        'Mail.Read',
+        'Tasks.ReadWrite',
+        'User.Read',
+    ],
+    adoScopes: ['vso.work'],
+};
